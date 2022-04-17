d3d2af3f2fabc38b434073ac5582aa73c1db4908
徐湛 <stevenhsu1998@gmail.com>
2022-04-18 00:45:15 +0800

    add css and modify course_by_instructor.html

--- frontend/course.py
+++ frontend/course.py
@@ -21,3 +21,10 @@ def show_instructor_home(instructor_id):
     courses_by_instructor = get_courses_by_instructor(instructor_id)
     return render_template('course/course_by_instructor.html', 
     instructor_id=instructor_id, courses=courses_by_instructor)
+
+# # Instructor edit course
+# @course_app.route('/instructor_home/editCourse')
+# def show_instructor_home(course_id):
+#     print(course_id)
+#     # courses_by_instructor = get_courses_by_instructor(instructor_id)
+#     return render_template('course/edit_course.html', course=course_id)
