a94f436739d644608f4dc361a38239ab9eb0a4a5
teacher144123 <teacher144123@gmail.com>
2022-04-20 23:32:09 +0800

    fix the completetion of contentes

--- backend/student.py
+++ backend/student.py
@@ -47,7 +47,7 @@ def insert_to_studentcontent(student_content):
 
 	sql_get = f'''
         SELECT * FROM STUDENTCONTENT
-        WHERE S_ID = {student_content['content_id']} AND CONTENT_ID = {student_content['s_id']}
+        WHERE S_ID = {student_content['s_id']} AND CONTENT_ID = {student_content['content_id']}
     '''
 	res_get = cursor.execute(sql_get)
 	cols = parse_column_headers(res_get)
@@ -62,6 +62,41 @@ def insert_to_studentcontent(student_content):
 		res = cursor.execute(sql)
 		connection.commit()
 		print("Insert into ENROLL success!")
+		return True
+	else: 
+		print("Already existed!")
+		return False
+
+def update_to_studentcontent(student_content):
+	cursor = connection.cursor()
+	now = datetime.now()
+	regist_time = now.strftime('%d-%m-%Y %H:%M:%S')
+	print(f'register time: {regist_time}')
+
+	sql_get = f'''
+        SELECT * FROM STUDENTCONTENT
+        WHERE S_ID = {student_content['s_id']} AND CONTENT_ID = {student_content['content_id']}
+    '''
+	res_get = cursor.execute(sql_get)
+	cols = parse_column_headers(res_get)
+	
+	stu_content = res_get.fetchone()
+
+	# if no record, then insert new one
+	if not stu_content:
+		print("No record found")
+		return False
+
+	stu_content = dict(zip(cols, stu_content))
+
+	if stu_content['STATUS'] == 'not yet':
+		sql = f'''
+			UPDATE STUDENTCONTENT
+			SET STATUS = 'finish', COMPLETE = TO_TIMESTAMP('{regist_time}', 'DD-MM-YYYY HH24:MI:SS')
+			WHERE S_ID = {student_content['s_id']} AND CONTENT_ID = {student_content['content_id']}
+		'''
+		res = cursor.execute(sql)
+		connection.commit()
 		return True
 	else: 
 		print("Already existed!")
